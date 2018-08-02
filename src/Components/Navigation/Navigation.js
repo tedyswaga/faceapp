3442a148b5028849af319a8db1af8819a0e0aa44
Teddy Mutuma <teddy@netsyncsolutions.co.ke>
2018-08-02 17:23:38 +0300

    all

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -8,7 +8,7 @@ const Navigation = ({onRouteChange, isSignedIn}) => {
 		
 			<nav style={{display:'flex', justifyContent:'flex-end'}}>
 				<p 
-				onClick={() => onRouteChange('signout')}
+				onClick={() => onRouteChange('signing')}
 				className='f3 link dim black underline pa3 pointer'>Sign Out </p>
 			</nav>
 		);
